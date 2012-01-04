5e0f4cbe06647709526a4067cfe63b8aa9e2f83b
Chris White <cwhite@sproutbox.com>
2012-01-03 20:12:21 -0500

    Initial rails-generated project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevotionCms::Application.config.secret_token = '2235952e9ef0bd0dfd4500614cd18baf64d0846187124f148d1ef495f72eb3214607fb566e25982e169aba9f0fbbe462e3cf9b8229f8bf3d6e392f2c35f79b79'
