cc4eded7fa829a0847f327e2705688c6a6c48aa8
Chris White <chris@chrislwhite.com>
2012-01-22 08:40:03 -0500

    Add days#show action

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,4 +2,10 @@ class DaysController < ApplicationController
   def index
     @days = DevotionDay.published
   end
+
+  def show
+    @day = DevotionDay.find(params[:id])
+
+    redirect to days_path unless @day
+  end
 end
