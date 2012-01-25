f08aee5ec4f724abaa725b6771afb510b5aeee07
Chris White <chris@chrislwhite.com>
2012-01-25 07:11:16 -0500

    Validate that something was put into the email field on subscription signup

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,7 @@ class Subscriber < ActiveRecord::Base
   before_save :generate_private_code
 
   validates_uniqueness_of :email
+  validates_presence_of :email
 
   def generate_private_code
     self.private_code = SecureRandom.hex(6)
