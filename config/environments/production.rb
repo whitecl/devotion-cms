43cb153bc55365b04517a238e4a5841a13cda525
Chris White <cwhite@sproutbox.com>
2012-02-04 16:25:58 -0500

    Production config fix for active admin styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ DevotionCms::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += ['active_admin.css', 'active_admin.js']
 
   # Generate digests for assets URLs
   config.assets.digest = true
