444fc47ca660f142ce838d5850fec7b574140fe3
Chris White <cwhite@sproutbox.com>
2012-02-04 14:27:00 -0500

    Typo in mailer host domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ DevotionCms::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'exowoship.com' }
+  config.action_mailer.default_url_options = { :host => 'exoworship.com' }
   config.action_mailer.asset_host = "http://www.exoworship.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
