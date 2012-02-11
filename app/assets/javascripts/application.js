c44150a6d03d454b0065ebce76aa52b0965ff833
Chad Burrus <cburrus@sentryds.com>
2012-02-10 23:58:45 -0500

    most of jamison's theme changes
    Still to do:
    1.  Double-check fonts/colors/actual sizes.  (Need Jamison (or his PSD) for this one.)
    2.  Swap out logo and exodus images (still temporary ones).
    3.  IE6:
        1.  Doesn't handle "margins : auto" correctly
        2.  Doesn't scale the images correctly
        3.  Needs a PNG fix
        4.  May be letting things hang over the edge on the FAQ page
    4.  Check other browsers (particularly IE7 and IE8)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,19 +13,27 @@
 
 // For navigation
 
-$(document).ready(function () {
-  bind_calendar_navigation();
-  $("#subcontainer > div").equalHeights(600);
-});
-
-function bind_calendar_navigation() {
-  $("#calendar_nav li").click(function() {
-    var target_season_id = $(this).data("season-id");
-
-    $('#calendar_nav li').removeClass('selected');
-    $(this).addClass('selected');
-
-    $('.season_day_box').hide();
-    $('#calendar-season-' + target_season_id).fadeIn();
+(function ($) {
+  $(document).ready(function () {
+    bind_calendar_navigation();
+    fix_heights();
   });
-}
+
+  $(window).resize(fix_heights);
+
+  function bind_calendar_navigation() {
+    $("#calendar_nav li").click(function() {
+      var target_season_id = $(this).data("season-id");
+
+      $('#calendar_nav li').removeClass('selected');
+      $(this).addClass('selected');
+
+      $('.season_day_box').hide();
+      $('#calendar-season-' + target_season_id).fadeIn();
+    });
+  }
+
+  function fix_heights() {
+    $("#subcontainer > div").equalHeights($(window).innerHeight());
+  }
+})(jQuery);
