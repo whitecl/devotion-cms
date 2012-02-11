8c87902fe418eba40b438345231651c605ff4c32
Chad Burrus <cburrus@sentryds.com>
2012-02-11 14:02:58 -0500

    fix the shutdown script

--- shutdown_vagrant.sh
+++ shutdown_vagrant.sh
@@ -7,7 +7,7 @@ vagrant halt
 #  Haven't tried the patch yet
 #
 echo "WARNING:  this will nuke your virtual disk, so you'll have to re-init your db, gems, and such if you use it."
-read "Destroy Vagrant Disk? [y/N] " DESTROY_DISK
+read -p "Destroy Vagrant Disk? [y/N] " DESTROY_DISK
 if [ "$DESTROY_DISK" == "y" ]
 then
 	vagrant destroy
