9341e00265e4fe21b1f06fde726d208800cd9781
Chris White <cwhite@sproutbox.com>
2012-02-22 20:19:03 -0500

    Add lambda to devotion day scopes so they update between days

--- app/models/devotion_day.rb
+++ app/models/devotion_day.rb
@@ -2,8 +2,8 @@ class DevotionDay < ActiveRecord::Base
   belongs_to :season
   has_many :devotions
 
-  scope :for_today, where(:publish_date => Date.today)
-  scope :published, where("publish_date <= \"#{Date.today}\"")
+  scope :for_today, lambda { where(:publish_date => Date.today) } 
+  scope :published, lambda { where("publish_date <= \"#{Date.today}\"") }
 
   def published?
     self.publish_date <= Date.today
