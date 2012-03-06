649660a68e2dad9a4478446ed2ad1e61d813ecc5
Chris White <cwhite@sproutbox.com>
2012-03-05 20:26:54 -0500

    Last minute email validity check before mail is sent

--- app/jobs/send_daily_email.rb
+++ app/jobs/send_daily_email.rb
@@ -8,7 +8,10 @@ class SendDailyEmail
     return unless devotion_day
 
     Subscriber.all.each do |subscriber|
-      DailyMailer.send_daily_devotionals(subscriber.id).deliver
+      # last minute email validity check
+      if subscriber.email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+        DailyMailer.send_daily_devotionals(subscriber.id).deliver
+      end
     end
   end
 end
