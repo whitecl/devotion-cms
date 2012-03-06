f275354ac9c25d278ee1fe9c6bca0a0d8c7a0029
Chris White <cwhite@sproutbox.com>
2012-03-05 20:26:35 -0500

    Validate valid email for new subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,10 +1,19 @@
 class Subscriber < ActiveRecord::Base
   before_save :generate_private_code
 
-  validates_uniqueness_of :email
-  validates_presence_of :email
+  validates :email, uniqueness: true, presence: true 
+  validate  :email_is_an_email
 
   def generate_private_code
     self.private_code = SecureRandom.hex(6)
+  end
+
+  def email_is_an_email
+    unless self.email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+      self.errors[:email] << 'Email is not a valid email'
+      false
+    else
+      true
+    end
   end
 end
